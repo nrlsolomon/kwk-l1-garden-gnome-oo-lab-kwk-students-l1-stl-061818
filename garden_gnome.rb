c266a3018c4dac8d7786a61316135f018b13b94f
nrlsolomon <nrlsolom5@gmail.com>
2018-06-21 20:47:45 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -1,1 +1,33 @@
+class Book
+  
+  attr_accessor :author, :titel, :genre, :length 
+  #makes a get,set for you.
+  
+  
+  def initialize(author,titel,genre,length)
+    @author=author
+    @titel=titel
+    @genre=genre
+    @length=length
+  end
+  
+end
+
+
+book1 = Book.new("susanne","hunger games","sci-fi",374)
+book2 = Book.new("louis","the giver","sci-fi",240)
+book3 = Book.new("jk","azkaban","fantasy",435)
+book4 = Book.new("raggs","children","fantasy",400)
+
+
+puts book4.author
+puts book4.titel
+puts book4.genre
+puts book4.length
+
+
+
+
+
+
 
